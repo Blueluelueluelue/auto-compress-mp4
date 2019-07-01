1cdc10a90065c8584f92a083a5743769613ce309
Blueluelueluelue <tahaking786@gmail.com>
2019-07-02 00:04:34 +0530

    can provide path of the folder containing mp4s

--- script.py
+++ script.py
@@ -1,20 +1,37 @@
 from os import *
 from os.path import *
 import ffmpy
+import sys
 
+videopath = getcwd()
+alreadycompressedprefix = 'done-'
+compressedfileprefix = 'compressed-'
 
-videopath = 'E:/Work/Call Recordings'
-compressedpath = videopath + '/Compressed'
-videofilesmp4 = [f for f in listdir(videopath) if isfile(join(videopath, f)) and f.endswith('.mp4') and not f.startswith('comp-')]
-print(videofilesmp4)
-crf = 25
-for file_name in videofilesmp4:
-    output_name = join(compressedpath, 'comp-' + file_name)
-    inp={join(videopath, file_name):None}
-    outp = {output_name:'-vcodec libx264 -crf %d'%crf}
-    ff=ffmpy.FFmpeg(inputs=inp,outputs=outp)
-    print(ff.cmd) # just to verify that it produces the correct ffmpeg command
-    ff.run()
-    rename(join(videopath, file_name), join(videopath, 'comp-' + file_name))
-    print('{0} renamed to comp-{0}'.format(file_name))
-print('done!')
+if len(sys.argv) >= 2:
+    videopath = sys.argv[1]
+    if not exists(videopath):
+        print('provided path doesn\'t exist, exiting...')
+        exit()
+compressedpath = videopath + '/Compressed'    
+
+if not exists(compressedpath):
+    mkdir(compressedpath)
+    print('Created a new directory {} where the compressed files will be saved'.format(compressedpath))
+
+try:
+    videofilesmp4 = [f for f in listdir(videopath) if isfile(join(videopath, f)) and f.endswith('.mp4') and not f.startswith(alreadycompressedprefix)]
+except:
+    print('something went wrong')
+else:
+    print(videofilesmp4)
+    crf = 25
+    for file_name in videofilesmp4:
+        output_name = join(compressedpath, compressedfileprefix + file_name)
+        inp={join(videopath, file_name):None}
+        outp = {output_name:'-vcodec libx264 -crf %d'%crf}
+        ff=ffmpy.FFmpeg(inputs=inp,outputs=outp)
+        print(ff.cmd) # just to verify that it produces the correct ffmpeg command
+        ff.run()
+        rename(join(videopath, file_name), join(videopath, alreadycompressedprefix + file_name))
+        print('{0} renamed to {1}{0}'.format(file_name,alreadycompressedprefix))
+    print('done!')
